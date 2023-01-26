c3cd3cddf6263939630dc548fb54e1930058eefa
lanhaidixingren <laihaidixingren@163.com>
2023-01-26 11:21:11 +0000

    上传文件至 ''

--- py_cctv_cs.py
+++ py_cctv_cs.py
@@ -177,6 +177,5 @@ class Spider(Spider):  # 元类 默认的元类 type
 
 	def localProxy(self,param):
 		return [200, "video/MP2T", action, ""]
-        def localProxuuy(self,param):
-		return [200, "video/MP2T", action, ""]
-        
+        def localProxuuy(self):
+		pass
