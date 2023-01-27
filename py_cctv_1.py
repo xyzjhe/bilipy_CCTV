bb085419e2a0eb9e52957e25c873a0c8af1451f1
lanhaidixingren <laihaidixingren@163.com>
2023-01-27 08:34:31 +0000

    上传文件至 ''

--- py_cctv_1.py
+++ py_cctv_1.py
@@ -3,15 +3,14 @@
 import sys
 sys.path.append('..') 
 from base.spider import Spider
-import requests
 import json
+import time
 import base64
 
-class Spider(Spider):
+class Spider(Spider):  # 元类 默认的元类 type
 	def getName(self):
-		return "CCTV片库"
+		return "央视片库"
 	def init(self,extend=""):
-		self.ali = extend[0]
 		print("============{0}============".format(extend))
 		pass
 	def isVideoFormat(self,url):
@@ -21,74 +20,114 @@ class Spider(Spider):
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-		    "电视剧": "1",
-		    "动画片": "2",
-		    "纪录片": "3",
-		    "特色节目": "4"
+			"电视剧": "1",
+			"动画片": "2",
+			"纪录片": "3",
+			"特色节目": "4"
 		}
 		classes = []
 		for k in cateManual:
-		    classes.append({
-			'type_name': k,
-			'type_id': cateManual[k]
-		    })
-
+			classes.append({
+				'type_name':k,
+				'type_id':cateManual[k]
+			})
 		result['class'] = classes
-		if (filter):
-		    result['filters'] = self.config['filter']
+		if(filter):
+			result['filters'] = self.config['filter']
 		return result
 	def homeVideoContent(self):
-		result = {}
-		return result
-	def categoryContent(self,tid,pg,filter,extend):
-		result = {}
-		return result
-	header = {
-		"User-Agent": "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36",
-		"origin": "https://www.upyunso.com/"
-	}
-	def detailContent(self,array):
-		id = array[0]
-		ysurl = 'https://api.upyunso.com/{0}'.format(id.replace('.html', ''))
-		header = {
-			"User-Agent": "Mozilla/5.0 (Linux; Android 12; V2049A Build/SP1A.210812.003; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36",
-			"referer": "https://www.upyunso.com/{0}".format(id),
-			"origin": "https://www.upyunso.com/"
+		result = {
+			'list':[]
 		}
-		urlrsp = requests.get(url=ysurl, headers=header).text.encode('ISO-8859-1').decode('UTF-8')
-		url = ["{0}".format(json.loads(base64.b64decode(urlrsp))['result']['res_url'])]
-		print(url)
-		return self.ali.detailContent(url)
+		return result
+	def categoryContent(self,tid,pg,filter,extend):		
+		pass
+	def detailContent(self,array):
+		aid = array[0].split('###')
+		tid = aid[0]
+		logo = aid[3]
+		lastVideo = aid[2]
+		title = aid[1]
+		date = aid[0]
+		if lastVideo == '_':
+			return {}
+
+		lastUrl = 'https://api.cntv.cn/video/videoinfoByGuid?guid={0}&serviceId=tvcctv'.format(lastVideo)
+		lastJo = self.fetch(lastUrl,headers=self.header).json()
+		topicId = lastJo['ctid']
+		url = "https://api.cntv.cn/NewVideo/getVideoListByColumn?id={0}&d={1}&p=1&n=100&sort=desc&mode=0&serviceId=tvcctv&t=json".format(topicId,date)
+		jo = self.fetch(url,headers=self.header).json()
+		vodList = jo['data']['list']
+		videoList = []
+		for video in vodList:
+			videoList.append(video['title']+"$"+video['guid'])
+		if len(videoList) == 0:
+			return {}
+		if len(date) == 0:
+			date = time.strftime("%Y", time.localtime(time.time()))
+		vod = {
+			"vod_id":array[0],
+			"vod_name":date +" "+title,
+			"vod_pic":logo,
+			"type_name":lastJo['channel'],
+			"vod_year":date,
+			"vod_area":"",
+			"vod_remarks":date,
+			"vod_actor":"",
+			"vod_director":topicId,
+			"vod_content":"当前页面默认只展示最新100期的内容，可在分类页面选择年份和月份进行往期节目查看。年份和月份仅影响当前页面内容，不参与分类过滤。视频默认播放可以获取到的最高帧率。"
+		}
+
+		vod['vod_play_from'] = 'CCTV'
+		vod['vod_play_url'] = "#".join(videoList)
+		result = {
+			'list':[
+				vod
+			]
+		}
+		return result
 
 	def searchContent(self,key,quick):
-		url = "https://api.upyunso.com/search?keyword={0}&page=1&s_type=2".format(key)
-		rsp = requests.get(url=url, headers=self.header)
-		vodList = json.loads(base64.b64decode(rsp.text))['result']['items']
-		videos = []
-		for vod in vodList:
-			conList = vod['content']
-			for con in conList:
-				if 'download.html?url' in con['size']:
-					vid = con['size']
-					videos.append({
-						"vod_id": vid,
-						"vod_name": con['title'],
-						"vod_pic": "https://inews.gtimg.com/newsapp_bt/0/13263837859/1000",
-						"vod_remarks": vod['insert_time']
-					})
 		result = {
-			'list':videos
+			'list':[]
 		}
 		return result
-
 	def playerContent(self,flag,id,vipFlags):
-		return self.ali.playerContent(flag,id,vipFlags)
+		result = {}
+		url = "https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={0}".format(id)
+		jo = self.fetch(url,headers=self.header).json()
+		link = jo['hls_url'].strip()
+		rsp = self.fetch(link,headers=self.header)
+		content = rsp.text.strip()
+		arr = content.split('\n')
+		urlPrefix = self.regStr(link,'(http[s]?://[a-zA-z0-9.]+)/')
+
+		subUrl = arr[-1].split('/')
+		subUrl[3] = '1200'
+		subUrl[-1] = '1200.m3u8'
+		hdUrl = urlPrefix + '/'.join(subUrl)
+
+		url = urlPrefix + arr[-1]
+
+		hdRsp = self.fetch(hdUrl,headers=self.header)
+		if hdRsp.status_code == 200:
+			url = hdUrl
+
+		result["parse"] = 0
+		result["playUrl"] = ''
+		result["url"] = url
+		result["header"] = ''
+		return result
 
 	config = {
 		"player": {},
-		"filter": {}
+		"filter": {"CCTV":[{"key":"cid","name":"频道","value":[{"n":"全部","v":""},{"n":"CCTV-1综合","v":"EPGC1386744804340101"},{"n":"CCTV-2财经","v":"EPGC1386744804340102"},{"n":"CCTV-3综艺","v":"EPGC1386744804340103"},{"n":"CCTV-4中文国际","v":"EPGC1386744804340104"},{"n":"CCTV-5体育","v":"EPGC1386744804340107"},{"n":"CCTV-6电影","v":"EPGC1386744804340108"},{"n":"CCTV-7国防军事","v":"EPGC1386744804340109"},{"n":"CCTV-8电视剧","v":"EPGC1386744804340110"},{"n":"CCTV-9纪录","v":"EPGC1386744804340112"},{"n":"CCTV-10科教","v":"EPGC1386744804340113"},{"n":"CCTV-11戏曲","v":"EPGC1386744804340114"},{"n":"CCTV-12社会与法","v":"EPGC1386744804340115"},{"n":"CCTV-13新闻","v":"EPGC1386744804340116"},{"n":"CCTV-14少儿","v":"EPGC1386744804340117"},{"n":"CCTV-15音乐","v":"EPGC1386744804340118"},{"n":"CCTV-16奥林匹克","v":"EPGC1634630207058998"},{"n":"CCTV-17农业农村","v":"EPGC1563932742616872"},{"n":"CCTV-5+体育赛事","v":"EPGC1468294755566101"}]},{"key":"fc","name":"分类","value":[{"n":"全部","v":""},{"n":"新闻","v":"新闻"},{"n":"体育","v":"体育"},{"n":"综艺","v":"综艺"},{"n":"健康","v":"健康"},{"n":"生活","v":"生活"},{"n":"科教","v":"科教"},{"n":"经济","v":"经济"},{"n":"农业","v":"农业"},{"n":"法治","v":"法治"},{"n":"军事","v":"军事"},{"n":"少儿","v":"少儿"},{"n":"动画","v":"动画"},{"n":"纪实","v":"纪实"},{"n":"戏曲","v":"戏曲"},{"n":"音乐","v":"音乐"},{"n":"影视","v":"影视"}]},{"key":"fl","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010","v":"2010"},{"n":"2009","v":"2009"},{"n":"2008","v":"2008"},{"n":"2007","v":"2007"},{"n":"2006","v":"2006"},{"n":"2005","v":"2005"},{"n":"2004","v":"2004"},{"n":"2003","v":"2003"},{"n":"2002","v":"2002"},{"n":"2001","v":"2001"},{"n":"2000","v":"2000"}]},{"key":"month","name":"月份","value":[{"n":"全部","v":""},{"n":"12","v":"12"},{"n":"11","v":"11"},{"n":"10","v":"10"},{"n":"09","v":"09"},{"n":"08","v":"08"},{"n":"07","v":"07"},{"n":"06","v":"06"},{"n":"05","v":"05"},{"n":"04","v":"04"},{"n":"03","v":"03"},{"n":"02","v":"02"},{"n":"01","v":"01"}]}]}
 	}
-	header = {}
+	header = {
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
+		"Origin": "https://tv.cctv.com",
+		"Referer": "https://tv.cctv.com/"
+	}
 
 	def localProxy(self,param):
 		return [200, "video/MP2T", action, ""]
