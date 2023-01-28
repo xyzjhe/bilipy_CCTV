f0217b3ee28d359db37d6ea2d9f4869172751e7e
lanhaidixingren <laihaidixingren@163.com>
2023-01-28 06:28:38 +0000

    上传文件至 ''

--- py_cctv_1.py
+++ py_cctv_1.py
@@ -9,7 +9,7 @@ import base64
 
 class Spider(Spider):  # 元类 默认的元类 type
 	def getName(self):
-		return "央视片库"
+		return "央视大全"
 	def init(self,extend=""):
 		print("============{0}============".format(extend))
 		pass
