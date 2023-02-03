2570fbc53203a62cc8f2a56e178123c7c54a65ec
lanhaidixingren <laihaidixingren@163.com>
2023-02-03 03:32:37 +0000

    上传文件至 ''

--- py_6vFilm.py
+++ py_6vFilm.py
@@ -21,7 +21,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"动画片19": "donghuapian",
+			"动画片20": "donghuapian",
 			"科幻片": "kehuanpian",
 			"爱情片": "aiqingpian",
 			"动作片": "dongzuopian",
@@ -152,6 +152,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 		rsp = self.fetch(id)
 		htmlTxt=rsp.text
 		pattern = re.compile(r'allowfullscreen=".+"\s*.*src="(.+?)">')
+		if ListRe==[]:
+			return result
 		result["parse"] = 0
 		result["playUrl"] = r''
 		result["url"] = r"https://audio.qtfm.cn/audiostream/redirect/288247/11871891?device_id=MOBILESITE&t=1675394744107&sign=4ccb19e8a6aed972f4b24757c9ce7567"
