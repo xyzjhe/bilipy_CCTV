44ea84d3b7fc7d28fad68a6caf1013e11d2cb334
lanhaidixingren <laihaidixingren@163.com>
2023-02-03 04:50:45 +0000

    上传文件至 ''

--- py_6vFilm.py
+++ py_6vFilm.py
@@ -21,7 +21,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"动画片6": "donghuapian",
+			"动画片7": "donghuapian",
 			"科幻片": "kehuanpian",
 			"爱情片": "aiqingpian",
 			"动作片": "dongzuopian",
@@ -154,7 +154,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 		pattern =re.search( r'allowfullscreen=".+"\s*.*src="(.+?)">', htmlTxt, re.M|re.I).group()
 		if len(pattern)<4:
 			return result
-		rsp = self.fetch("https://v8.dious.cc/share/AMwr3JnG88P8WLJ6")
+		rsp = self.fetch(pattern)
 		htmlTxt=rsp.text
 		head=re.search( r'(https://.+?cc)', "https://v8.dious.cc/share/AMwr3JnG88P8WLJ6", re.M|re.I).group()
 		if len(head)<4:
