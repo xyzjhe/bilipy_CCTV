1731d0e83d8b23d25152240eef15507bfc9001f9
lanhaidixingren <laihaidixingren@163.com>
2023-02-03 02:41:21 +0000

    上传文件至 ''

--- py_6vFilm.py
+++ py_6vFilm.py
@@ -21,7 +21,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"动画片10": "donghuapian",
+			"动画片11": "donghuapian",
 			"科幻片": "kehuanpian",
 			"爱情片": "aiqingpian",
 			"动作片": "dongzuopian",
