9dea1b02a255f1f14683e303dd77f1ab8c80a748
lanhaidixingren <laihaidixingren@163.com>
2023-02-05 02:04:15 +0000

    上传文件至 ''

--- py_6vFilm.py
+++ py_6vFilm.py
@@ -21,7 +21,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"科幻片9": "kehuanpian",
+			"科幻片10": "kehuanpian",
 			"动画片": "donghuapian",
 			"电视剧": "dianshiju",
 			"爱情片": "aiqingpian",
@@ -151,13 +151,12 @@ class Spider(Spider):  # 元类 默认的元类 type
 		result = {}
 		rsp = self.fetch(id)
 		htmlTxt=rsp.text
-		pattern=re.compile( r"(https{0,1}://.+?\.m3u8.*?)", htmlTxt)
-		ListRe=pattern.findall(htmlTxt)
+		pattern=re.search( r'(https{0,1}://.+?\.m3u8.*?)', htmlTxt, re.M|re.I).group(1)
 		url=""
-		if ListRe==[]:	
+		if len(pattern)<6:	
 			url=self.get_playUrlMethodOne(html=htmlTxt)
 		else:
-			url=ListRe[0]
+			url=pattern
 		result["parse"] = 0
 		result["playUrl"] =""
 		result["url"] = url
