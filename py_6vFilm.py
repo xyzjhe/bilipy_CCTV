81297d1ebee9e562c77306d73864450e1ff6543b
lanhaidixingren <laihaidixingren@163.com>
2023-02-05 02:42:39 +0000

    上传文件至 ''

--- py_6vFilm.py
+++ py_6vFilm.py
@@ -21,7 +21,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"科幻片15": "kehuanpian",
+			"科幻片16": "kehuanpian",
 			"动画片": "donghuapian",
 			"电视剧": "dianshiju",
 			"爱情片": "aiqingpian",
@@ -151,7 +151,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 		result = {}
 		rsp = self.fetch(id)
 		htmlTxt=rsp.text
-		pattern=re.search( r'(https{0,1}://.+?\.m3u8.*?)', htmlTxt, re.M|re.I).group(1)
+		pattern=re.compile(r'(https{0,1}://.+?\.m3u8.*?)')
+		ListRe=pattern.findall(htmlTxt)
 		url=""
 		#if len(pattern)<6:	
 		url=self.get_playUrlMethodOne(html=htmlTxt)
