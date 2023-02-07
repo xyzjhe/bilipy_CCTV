ced34927e4a945564deaa817d3cba5ef46f5d03d
lanhaidixingren <laihaidixingren@163.com>
2023-02-07 10:17:41 +0000

    上传文件至 ''

--- py_6vFilm_1.py
+++ py_6vFilm_1.py
@@ -24,7 +24,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"科幻片28": "kehuanpian",
+			"科幻片27": "kehuanpian",
 			"动画片": "donghuapian",
 			"电视剧": "dianshiju",
 			"爱情片": "aiqingpian",
@@ -189,8 +189,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 			#title =soup.sub('', vod[1])
 			if len(lastVideo) == 0:
 				lastVideo = '_'
-			if lastVideo.find(head)<0 and lastVideo!="_":
-				lastVideo=head+lastVideo
+			#if lastVideo.find(head)<0 and lastVideo!="_":
+			lastVideo=head+lastVideo
 			guid = tid+'###'+vod[1]+'###'+lastVideo+'###'+vod[2]
 			title =vod[1]
 			img = vod[2]
