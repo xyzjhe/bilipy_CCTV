5599ab477687f4f991aad1f08692fad6a88b8580
lanhaidixingren <laihaidixingren@163.com>
2023-02-23 06:33:18 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态3": "动态",
+                "动态4": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -448,42 +448,10 @@ class Spider(Spider):  # 元类 默认的元类 type
         return str.replace('\n', '').replace('\t', '').replace('\r', '').replace(' ', '')
 
     def detailContent(self, array):
-        aid = array[0]
-        url = "https://api.bilibili.com/x/web-interface/view?aid={0}".format(aid)
-        rsp = self.fetch(url, headers=self.header)
-        jRoot = json.loads(rsp.text)
-        jo = jRoot['data']
-        title = jo['title'].replace("<em class=\"keyword\">", "").replace("</em>", "")
-        pic = jo['pic']
-        desc = jo['desc']
-        timeStamp = jo['pubdate']
-        timeArray = time.localtime(timeStamp)
-        year = str(time.strftime("%Y", timeArray))
-        dire = jo['owner']['name']
-        typeName = jo['tname']
-        remark = str(jo['duration']).strip()
-        vod = {
-            "vod_id": aid,
-            "vod_name": title,
-            "vod_pic": pic,
-            "type_name": typeName,
-            "vod_year": year,
-            "vod_area": "",
-            "vod_remarks": remark,
-            "vod_actor": "",
-            "vod_director": dire,
-            "vod_content": desc
-        }
-        ja = jo['pages']
-        playUrl = ''
-        for tmpJo in ja:
-            cid = tmpJo['cid']
-            part = tmpJo['part'].replace("#", "-")
-            playUrl = playUrl + '{0}${1}_{2}#'.format(part, aid, cid)
-
+        aaid1 = array[0]
+        playUrl=self.get_list(aid=aid1)
         vod['vod_play_from'] = 'B站视频'
         vod['vod_play_url'] = playUrl
-
         result = {
             'list': [
                 vod
