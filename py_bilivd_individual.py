d3eb7ab38daadb5327c393b8950756543e89e0e9
lanhaidixingren <laihaidixingren@163.com>
2023-02-24 07:24:12 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态17": "动态",
+                "动态19": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -509,21 +509,21 @@ class Spider(Spider):  # 元类 默认的元类 type
         typeName = ''
         remark = ''
         vod = {
-            "vod_id": aid,
+            "vod_id": '999',
             "vod_name": title,
             "vod_pic": pic,
             "type_name": typeName,
-            "vod_year": year,
+            "vod_year": '',
             "vod_area": "",
-            "vod_remarks": remark,
+            "vod_remarks": '',
             "vod_actor": "",
-            "vod_director": dire,
-            "vod_content": desc
+            "vod_director": '',
+            "vod_content": ''
         }
         playUrl = ''
         for tmpJo in jo:
             cid = tmpJo['title']
-            part = tmpJo['bvid'].replace("#", "-")
+            part = tmpJo['bvid']
             playUrl = playUrl + '{0}${1}#'.format(part, aid)
 
         vod['vod_play_from'] = 'B站视频'
