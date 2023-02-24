407b45f92b80d5ca4cbd0751b353cf6d6faaf9b1
lanhaidixingren <laihaidixingren@163.com>
2023-02-24 08:05:30 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态18": "动态",
+                "动态19": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -449,7 +449,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 
     def detailContent(self, array):
         result = {}
-        if array[0].find('###')<1:
+        if array[0].find('###')==100:
             result = self.get_list(aid=array[0])
         else:
             result = self.get_list_pu(aid=array[0])		
