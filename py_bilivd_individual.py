0ee98388a8af563d216d2517c95b4f65c4ae0057
lanhaidixingren <laihaidixingren@163.com>
2023-02-27 04:11:00 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -51,7 +51,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态[测试取播放地址28]": "动态",
+                "动态[测试取播放地址29]": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -634,11 +634,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         result["contentType"] = 'video/x-flv'
         return result
     def playerContent(self, flag, id, vipFlags):
-        result = {}
-        if id.find('bvid:')>0:
-            result=self.get_Url_pu(idTxt=id)
-        else:
-            result=self.get_Url(idTxt=id)
+        result = self.get_Url(idTxt=id)
         return result
 
     config = {
