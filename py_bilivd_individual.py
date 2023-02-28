78e89c2f7dce4e1098617f505f76c3d2f518aa23
lanhaidixingren <laihaidixingren@163.com>
2023-02-28 03:27:21 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态[测试取播放地址39]": "动态",
+                "动态[测试取播放地址40]": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -666,7 +666,9 @@ class Spider(Spider):  # 元类 默认的元类 type
             result = self.get_Url_pu(idTxt=id)
         if result=={}:
             url='https://m.bilibili.com/video/{0}'.format(mark)
-            header= {}
+            header= {
+                "Referer": "https://www.bilibili.com"
+            }
         return result
 
     config = {
