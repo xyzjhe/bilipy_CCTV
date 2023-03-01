15d7ed6faf414f8712da7edd03a2e3061bd93337
lanhaidixingren <laihaidixingren@163.com>
2023-03-01 06:45:04 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态[测试取播放地址51]": "动态",
+                "动态[测试取播放地址52]": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -658,8 +658,8 @@ class Spider(Spider):  # 元类 默认的元类 type
         result={}
         mark=''#正常
         if id.find('bvid:')<0:
-            mark=idTxt.id("_")[0]
-            result = self.get_Url(idTxt=id)
+            pass
+        result = self.get_Url(idTxt=id)
         return result
 
     config = {
