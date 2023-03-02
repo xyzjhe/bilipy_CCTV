815d2437a1f20c8e44aceadbeceae0dffcf5108a
lanhaidixingren <laihaidixingren@163.com>
2023-03-02 02:17:36 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态[测试取播放地址73]": "动态",
+                "动态[测试取播放地址74]": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -684,7 +684,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if Regex is None:
             returnTxt="https://cn-lnsy-cm-01-05.bilivideo.com/upgcxcode/57/22/63822257/63822257-1-6.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1677727745&gen=playurlv2&os=bcache&oi=1881581469&trid=00003a788397581d44038017616722851910h&mid=321534564&platform=html5&upsig=ec8d8283c859e25cb67ee48373422ea0&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&cdnid=3246&bvc=vod&nettype=0&bw=30495&logo=80000000"
         else:
-            returnTxt="https:"+Regex.group(Index)
+            returnTxt="https://cn-lnsy-cm-01-05.bilivideo.com/upgcxcode/57/22/63822257/63822257-1-6.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1677727745&gen=playurlv2&os=bcache&oi=1881581469&trid=00003a788397581d44038017616722851910h&mid=321534564&platform=html5&upsig=ec8d8283c859e25cb67ee48373422ea0&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&cdnid=3246&bvc=vod&nettype=0&bw=30495&logo=80000000"
         return returnTxt
     config = {
         "player": {},
