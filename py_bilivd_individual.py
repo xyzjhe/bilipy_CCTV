7d3e7295477c3b55b71d2a147db6529aef07ed34
lanhaidixingren <laihaidixingren@163.com>
2023-03-02 00:51:24 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态[测试取播放地址56]": "动态",
+                "动态[测试取播放地址57]": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -588,6 +588,8 @@ class Spider(Spider):  # 元类 默认的元类 type
             self.getCookie()
         rsp = self.fetch(url, cookies=self.cookies)
         jRoot = json.loads(rsp.text)
+        if jRoot['code']==-404:
+            return result
         jo = jRoot['data']
         ja = jo['durl']
 
