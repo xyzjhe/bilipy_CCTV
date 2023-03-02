19a98541f3030fbb2877e2fdf04a478b78e93a02
lanhaidixingren <laihaidixingren@163.com>
2023-03-02 00:22:11 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -52,7 +52,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         if self.login is True:
             cateManual = {
                 "频道": "频道",
-                "动态[测试取移动端地址(1)55]": "动态",
+                "动态[测试取播放地址55]": "动态",
                 "pu主": "pu主",
                 "热门": "热门",
                 "推荐": "推荐",
@@ -658,24 +658,11 @@ class Spider(Spider):  # 元类 默认的元类 type
         result={}
         mark=''
         if id.find('bvid:')<0:
-            mark=id.split("_")[0]
+            mark=id.split("_")[0]#正常
             result = self.get_Url(idTxt=id)
         else:
             mark=id.split(":")[0]
-            result = self.get_Url_pu(idTxt=id)#正常
-                if result=={}:
-            url='https://m.bilibili.com/video/{0}'.format(mark)
-            header= {
-                "Referer": "https://www.bilibili.com",
-                "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3947.100 Mobile Safari/537.36"
-            }
-            rsp = self.fetch(url, headers=header)
-            html=rsp.text
-            url='https:'+re.search(r'"readyVideoUrl":"(.+?)",', html, re.M|re.S).group(1)
-            result["parse"] = 0
-            result["playUrl"] = ''
-            result["url"] = url
-            result["header"] = header
+            result = self.get_Url_pu(idTxt=id)
         return result
 
     config = {
