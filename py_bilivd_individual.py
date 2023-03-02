a97c826eb6b906e4b8dbc67bb79fa9e70eacc989
lanhaidixingren <laihaidixingren@163.com>
2023-03-02 00:44:59 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -663,7 +663,7 @@ class Spider(Spider):  # 元类 默认的元类 type
         else:
             mark=id.split(":")[0]
             result = self.get_Url_pu(idTxt=id)#正常
-                if result=={}:
+        if result=={}:
             url='https://m.bilibili.com/video/{0}'.format(mark)
             header= {
                 "Referer": "https://www.bilibili.com",
