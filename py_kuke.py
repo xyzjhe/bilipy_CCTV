5d4e40bf58131492af362f92c0c545257146c196
lanhaidixingren <laihaidixingren@163.com>
2023-03-04 01:20:15 +0000

    上传文件至 ''

--- py_kuke.py
+++ py_kuke.py
@@ -1,73 +1,73 @@
 #coding=utf-8
- #!/usr/bin/python
- import sys
- sys.path.append('..')
- from base.spider import Spider
- import base64
- import math
- import json
- import requests
- import urllib
- from urllib import request, parse
- import urllib.request
- import re
- 
- class Spider(Spider):
- 	def getName(self):
- 		return "酷客影院"
- 	def init(self,extend=""):
- 		pass
- 	def isVideoFormat(self,url):
- 		pass
- 	def manualVideoCheck(self):
- 		pass
- 	def homeContent(self,filter):
- 		result = {}
- 		cateManual = {
- 			"电影": "dianying",
- 			"电视剧": "lianxuju",
- 			"综艺": "zongyi",
- 			"动漫": "dongman",
- 			"美女写真":"wuyejuchang"
- 		}
- 		classes = []
- 		for k in cateManual:
- 			classes.append({
- 				'type_name': k,
- 				'type_id': cateManual[k]
- 			})
- 
- 		result['class'] = classes
- 		if (filter):
- 			result['filters'] = self.config['filter']
- 		return result
- 	def homeVideoContent(self):
- 		rsp = self.fetch('http://www.meheme.com/')
- 		htmlTxt = rsp.text
- 		videos = self.get_list(html=htmlTxt)
- 		result = {
- 			'list': videos
- 		}
- 		return result
- 
- 	def categoryContent(self,tid,pg,filter,extend):
- 		result = {}
- 		url = 'http://www.meheme.com/vodshow/{0}--------{1}---.html'.format(tid,pg)
- 		rsp = self.fetch(url)
- 		htmlTxt=rsp.text
- 		videos = self.get_list(html=htmlTxt)
- 		pag=self.get_RegexGetText(Text=htmlTxt,RegexText=r'-(\d+?)---.html"\sclass="page-link page-next"\stitle="尾页">',Index=1)
- 		if pag=="":
- 			pag=1
- 		numvL = len(videos)
- 		result['list'] = videos
- 		result['page'] = pg
- 		result['pagecount'] = pag
- 		result['limit'] = numvL
- 		result['total'] = numvL
- 		return result
- 
- 	def detailContent(self,array):
+#!/usr/bin/python
+import sys
+sys.path.append('..')
+from base.spider import Spider
+import base64
+import math
+import json
+import requests
+import urllib
+from urllib import request, parse
+import urllib.request
+import re
+
+class Spider(Spider):
+	def getName(self):
+		return "酷客影院"
+	def init(self,extend=""):
+		pass
+	def isVideoFormat(self,url):
+		pass
+	def manualVideoCheck(self):
+		pass
+	def homeContent(self,filter):
+		result = {}
+		cateManual = {
+			"电影1": "dianying",
+			"电视剧": "lianxuju",
+			"综艺": "zongyi",
+			"动漫": "dongman",
+			"美女写真":"wuyejuchang"
+		}
+		classes = []
+		for k in cateManual:
+			classes.append({
+				'type_name': k,
+				'type_id': cateManual[k]
+			})
+
+		result['class'] = classes
+		if (filter):
+			result['filters'] = self.config['filter']
+		return result
+	def homeVideoContent(self):
+		rsp = self.fetch('http://www.meheme.com/')
+		htmlTxt = rsp.text
+		videos = self.get_list(html=htmlTxt)
+		result = {
+			'list': videos
+		}
+		return result
+
+	def categoryContent(self,tid,pg,filter,extend):
+		result = {}
+		url = 'http://www.meheme.com/vodshow/{0}--------{1}---.html'.format(tid,pg)
+		rsp = self.fetch(url,headers=self.header)
+		htmlTxt=rsp.text
+		videos = self.get_list(html=htmlTxt)
+		pag=self.get_RegexGetText(Text=htmlTxt,RegexText=r'-(\d+?)---.html"\sclass="page-link page-next"\stitle="尾页">',Index=1)
+		if pag=="":
+			pag=999
+		numvL = len(videos)
+		result['list'] = videos
+		result['page'] = pg
+		result['pagecount'] = pgc
+		result['limit'] = numvL
+		result['total'] = numvL
+		return result
+
+	def detailContent(self,array):
  		aid = array[0]
  		result={}
  		url='http://www.meheme.com{0}'.format(aid)
@@ -123,89 +123,74 @@
  			]
  		}
  		return result
- 
- 	def verifyCode(self):
- 		retry = 10
- 		header = {
- 			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
- 		while retry:
- 			try:
- 				session = requests.session()
- 				img = session.get('https://ikan6.vip/index.php/verify/index.html?', headers=header).content
- 				code = session.post('https://api.nn.ci/ocr/b64/text', data=base64.b64encode(img).decode()).text
- 				res = session.post(url=f"https://ikan6.vip/index.php/ajax/verify_check?type=search&verify={code}",
- 								   headers=header).json()
- 				if res["msg"] == "ok":
- 					return session
- 			except Exception as e:
- 				print(e)
- 			finally:
- 				retry = retry - 1
- 
- 	def searchContent(self,key,quick):
- 		Url='http://www.meheme.com/vodsearch/-------------.html?wd={0}&submit='.format(urllib.parse.quote(key))
- 		rsp = self.fetch(Url)
- 		htmlTxt = rsp.text
- 		videos = self.get_list(html=htmlTxt)
- 		result = {
- 				'list': videos
- 			}
- 		return result
- 
- 	def playerContent(self,flag,id,vipFlags):
- 		result = {}
- 		parse=1
- 		Url='http://www.meheme.com{0}'.format(id)
- 		rsp = self.fetch(Url)
- 		htmlTxt = rsp.text
- 		m3u8Line=self.get_RegexGetTextLine(Text=htmlTxt,RegexText=r'url":"(h.+?)",',Index=1)
- 		if len(m3u8Line)>0:
- 			Url=m3u8Line[0].replace("/","")
- 			parse=0 if Url.find('.m3u8')>1 else 1
- 		result["parse"] = parse
- 		result["playUrl"] = ''
- 		result["url"] = Url
- 		result["header"] = ''
- 		return result
- 	def get_RegexGetText(self,Text,RegexText,Index):
- 		returnTxt=""
- 		Regex=re.search(RegexText, Text, re.M|re.I)
- 		if Regex is None:
- 			returnTxt=""
- 		else:
- 			returnTxt=Regex.group(Index)
- 		return returnTxt	
- 	def get_RegexGetTextLine(self,Text,RegexText,Index):
- 		returnTxt=[]
- 		pattern = re.compile(RegexText)
- 		ListRe=pattern.findall(Text)
- 		if len(ListRe)<1:
- 			return returnTxt
- 		for value in ListRe:
- 			returnTxt.append(value)	
- 		return returnTxt
- 	def get_playlist(self,Text,headStr,endStr):
- 		circuit=""
- 		origin=Text.find(headStr)
- 		if origin>8:
- 			end=Text.find(endStr,origin)
- 			circuit=Text[origin:end]
- 		return circuit
- 	def removeHtml(self,txt):
- 		soup = re.compile(r'<[^>]+>',re.S)
- 		txt =soup.sub('', txt)
- 		return txt.replace("&nbsp;"," ")
- 	def get_webReadFile(self,urlStr):
- 		headers = {
- 			'Referer':urlStr,
- 			'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
- 			'Host': 'www.kukemv.com'
- 		}
- 		req = urllib.request.Request(url=urlStr, headers=headers)
- 		html = urllib.request.urlopen(req).read().decode('utf-8')
- 		return html
- 	def get_list(self,html):
- 		patternTxt=r'<a class="vodlist_thumb lazyload" href="(.+?)" title="(.+?)" data-original="(.+?)"'
+
+	def verifyCode(self):
+		pass
+
+	def searchContent(self,key,quick):
+		Url='http://www.meheme.com/vodsearch/-------------.html?wd={0}&submit='.format(urllib.parse.quote(key))
+		rsp = self.fetch(Url)
+		htmlTxt = rsp.text
+		videos = self.get_list(html=htmlTxt)
+		result = {
+				'list': videos
+			}
+		return result
+
+	def playerContent(self,flag,id,vipFlags):
+		result = {}
+		parse=1
+		Url='http://www.meheme.com{0}'.format(id)
+		rsp = self.fetch(Url)
+		htmlTxt = rsp.text
+		m3u8Line=self.get_RegexGetTextLine(Text=htmlTxt,RegexText=r'url":"(h.+?)",',Index=1)
+		if len(m3u8Line)>0:
+			Url=m3u8Line[0].replace("/","")
+			parse=0 if Url.find('.m3u8')>1 else 1
+		result["parse"] = parse
+		result["playUrl"] = ''
+		result["url"] = Url
+		result["header"] = ''
+		return result
+	def get_RegexGetText(self,Text,RegexText,Index):
+		returnTxt=""
+		Regex=re.search(RegexText, Text, re.M|re.I)
+		if Regex is None:
+			returnTxt=""
+		else:
+			returnTxt=Regex.group(Index)
+		return returnTxt	
+	def get_RegexGetTextLine(self,Text,RegexText,Index):
+		returnTxt=[]
+		pattern = re.compile(RegexText)
+		ListRe=pattern.findall(Text)
+		if len(ListRe)<1:
+			return returnTxt
+		for value in ListRe:
+			returnTxt.append(value)	
+		return returnTxt
+	def get_playlist(self,Text,headStr,endStr):
+		circuit=""
+		origin=Text.find(headStr)
+		if origin>8:
+			end=Text.find(endStr,origin)
+			circuit=Text[origin:end]
+		return circuit
+	def removeHtml(self,txt):
+		soup = re.compile(r'<[^>]+>',re.S)
+		txt =soup.sub('', txt)
+		return txt.replace("&nbsp;"," ")
+	def get_webReadFile(self,urlStr):
+		headers = {
+			'Referer':urlStr,
+			'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+			'Host': 'www.kukemv.com'
+		}
+		req = urllib.request.Request(url=urlStr, headers=headers)
+		html = urllib.request.urlopen(req).read().decode('utf-8')
+		return html
+	def get_list(self,html):
+		 		patternTxt=r'<a class="vodlist_thumb lazyload" href="(.+?)" title="(.+?)" data-original="(.+?)"'
  		pattern = re.compile(patternTxt)
  		ListRe=pattern.findall(html)
  		videos = []
@@ -222,22 +207,22 @@
  				"vod_remarks":''
  			})
  		return videos
- 	def get_lineList(self,Txt,mark,after):
- 		circuit=[]
- 		origin=Txt.find(mark)
- 		while origin>8:
- 			end=Txt.find(after,origin)
- 			circuit.append(Txt[origin:end])
- 			origin=Txt.find(mark,end)
- 		return circuit
- 	config = {
- 		"player": {},
- 		"filter": {}
- 	}
- 	header = {
- 		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
- 		'Host': 'www.kukemv.com'
- 	}
- 
- 	def localProxy(self,param):
- 		return [200, "video/MP2T", action, ""]
+	def get_lineList(self,Txt,mark,after):
+		circuit=[]
+		origin=Txt.find(mark)
+		while origin>8:
+			end=Txt.find(after,origin)
+			circuit.append(Txt[origin:end])
+			origin=Txt.find(mark,end)
+		return circuit
+	config = {
+		"player": {},
+		"filter": {}
+	}
+	header = {
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
+		'Host': 'www.kukemv.com'
+	}
+
+	def localProxy(self,param):
+		return [200, "video/MP2T", action, ""]
