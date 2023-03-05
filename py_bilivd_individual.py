93bb11f73ceae67c49a2b28e78b72ef031ab7aca
lanhaidixingren <laihaidixingren@163.com>
2023-03-05 09:24:43 +0000

    上传文件至 ''

--- py_bilivd_individual.py
+++ py_bilivd_individual.py
@@ -507,7 +507,7 @@ class Spider(Spider):  # 元类 默认的元类 type
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
         }
         videoList=[]
-        for i in range(1, 2):
+        for i in range(1, 3):
             url = "https://api.bilibili.com/x/space/arc/search?mid={0}&ps=30&tid=0&pn={1}&keyword=&order=pubdate&jsonp=jsonp".format(mid,i)
             rsp = self.fetch(url,headers=header)
             htmlTxt=rsp.text
