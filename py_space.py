c00b138600701a373591e1d6c55bec08b5bbc703
lanhaidixingren <laihaidixingren@163.com>
2023-03-14 07:43:20 +0000

    上传文件至 ''

--- py_space.py
+++ py_space.py
@@ -94,7 +94,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 		headers = {
 			'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3947.100 Mobile Safari/537.36'
 		}
-		result["parse"] = 2
+		result["parse"] = 1
 		result["playUrl"] = ''
 		result["url"] = id
 		result["header"] = headers	
