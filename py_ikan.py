7a13990c4844fd782fd683c96131072b49c0461a
lanhaidixingren <laihaidixingren@163.com>
2023-04-03 05:57:30 +0000

    上传文件至 ''

--- py_ikan.py
+++ py_ikan.py
@@ -7,10 +7,6 @@ import base64
 import math
 import json
 import requests
-import urllib
-from urllib import request, parse
-import urllib.request
-import re
 
 class Spider(Spider):
 	def getName(self):
@@ -48,30 +44,26 @@ class Spider(Spider):
 
 	def categoryContent(self,tid,pg,filter,extend):
 		result = {}
-		url = 'https://ikan6.vip/vodtype/{0}-{1}/'.format(tid,pg)
-		rsp = self.fetch(url)
-		htmlTxt=rsp.text
-		html = self.html(htmlTxt)
-		aList = html.xpath("//ul[contains(@class, 'myui-vodlist')]/li")
+		header = {
+			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
+		url = 'https://ikanys.tv/vodshow/{}--------{}---/'.format(tid,pg)
+		session= self.verifyCode('show')
+		rsp = session.get(url, headers=header)
+		html = self.html(rsp.text)
+		aList = html.xpath("//div[@class='module']/a")
 		videos = []
 		numvL = len(aList)
-		pgc = self.get_RegexGetText(Text=htmlTxt,RegexText=r'href="/vodtype/\d{1,3}-([0-9]+?)/">尾页</a>',Index=1)
-		if pgc=="":
-			pgc=999
+		pgc = math.ceil(numvL/15)
 		for a in aList:
-			aid = a.xpath("./div[contains(@class, 'myui-vodlist__box')]/a/@href")[0]
+			aid = a.xpath("./@href")[0]
 			aid = self.regStr(reg=r'/voddetail/(.*?)/', src=aid)
-			img = a.xpath(".//div[contains(@class, 'myui-vodlist__box')]/a/@data-original")[0]
-			name = a.xpath(".//div[contains(@class, 'myui-vodlist__box')]/a/@title")[0]
-			remark = a.xpath(".//span[contains(@class, 'pic-text text-right')]/text()")
-			if remark == []:
-				remark = a.xpath(".//span[contains(@class, 'pic-tag pic-tag-top')]/span/text()")
-			remark = remark[0]
+			img = a.xpath(".//div[@class='module-item-pic']/img/@data-original")[0]
+			name = a.xpath("./@title")[0]
 			videos.append({
 				"vod_id": aid,
 				"vod_name": name,
 				"vod_pic": img,
-				"vod_remarks": remark
+				"vod_remarks": ''
 			})
 		result['list'] = videos
 		result['page'] = pg
@@ -82,49 +74,39 @@ class Spider(Spider):
 
 	def detailContent(self,array):
 		aid = array[0]
-		url = 'https://ikan6.vip/voddetail/{0}/'.format(aid)
+		url = 'https://ikanys.tv/voddetail/{0}/'.format(aid)
 		rsp = self.fetch(url)
-		html = rsp.text
-		line=self.get_RegexGetTextLine(Text=html,RegexText=r'<a href="#(playlist[1-9]{1,8})"\s*.+?=".+?">(.+?)</a>',Index=1)
-		circuit=[]
-		for i in line:
-			circuit.append(self.get_playlist(Text=html,headStr='id="'+i[0],endStr="</div>"))
-		playFrom = []
-		videoList=[]
-		pattern = re.compile(r'<li class="col.+"><a class=".+" href="(.+?)">(.+?)</a></li>')
-		for v in circuit:
-			ListRe=pattern.findall(v)
-			vodItems = []
-			for value in ListRe:
-				vodItems.append(value[1]+"$"+value[0])
-			joinStr = "#".join(vodItems)
-			videoList.append(joinStr)
-		playFrom=[t[1] for t in line]
-		vod_play_from='$$$'.join(playFrom)
-		vod_play_url = "$$$".join(videoList)
-		title=self.get_RegexGetText(Text=html,RegexText=r'class="title">(.+?)</',Index=1)
-		pic=self.get_RegexGetText(Text=html,RegexText=r'data-original="(.+?)"',Index=1)
-		typeName=self.get_RegexGetText(Text=html,RegexText=r'<a href=".+?-----------/">(.+?)</a>',Index=1)
-		year=self.get_RegexGetText(Text=html,RegexText=r'<a href=".+?[0-9]{4}/">([0-9]{4}.*?)</a>',Index=1)
-		area=self.get_RegexGetText(Text=html,RegexText=r'地区：</span><a href=".+?/">(.*?)</a>',Index=1)
-		act=self.get_RegexGetText(Text=html,RegexText=r'<span class="text-muted">主演：(.*?)</p>',Index=1)
-		dir=self.get_RegexGetText(Text=html,RegexText=r'<span class="text-muted">导演：(.*?)</p>',Index=1)
-		cont=self.get_RegexGetText(Text=html,RegexText=r'简介：</span>(.*?)"',Index=1)
-		print(cont)
+		html = self.html(rsp.text)
+		node = html.xpath("//div[@class='module-main']")[0]
+		title = node.xpath(".//div[@class='module-info-heading']/h1/text()")[0]
+		pic = html.xpath("//div[@class='module-item-pic']/img/@data-original")[0]
 		vod = {
 			"vod_id": aid,
 			"vod_name": title,
 			"vod_pic": pic,
-			"type_name": typeName,
-			"vod_year": year,
-			"vod_area": area,
+			"type_name": '',
+			"vod_year": '',
+			"vod_area": '',
 			"vod_remarks": '',
-			"vod_actor": self.removeHtml(txt=act),
-			"vod_director": self.removeHtml(txt=dir),
-			"vod_content": cont
+			"vod_actor": '',
+			"vod_director": '',
+			"vod_content": ''
 		}
-		vod['vod_play_from'] = vod_play_from
-		vod['vod_play_url'] = vod_play_url
+		playFrom = ''
+		playfromList = html.xpath("//div[@class='module-tab-items-box hisSwiper']/div")
+		for pL in playfromList:
+			pL = pL.xpath("./@data-dropdown-value")[0].strip()
+			playFrom = playFrom + '$$$' + pL
+		urlList = html.xpath("//div[contains(@class,'module-list sort-list tab-list his-tab-list')]")
+		playUrl = ''
+		for uL in urlList:
+			for playurl in uL.xpath(".//a"):
+				purl = self.regStr(reg=r'/vodplay/(.*?)/', src=playurl.xpath("./@href")[0])
+				name = playurl.xpath("./@title")[0]
+				playUrl = playUrl + '{}${}#'.format(name, purl)
+			playUrl = playUrl + '$$$'
+		vod['vod_play_from'] = playFrom.strip('$$$')
+		vod['vod_play_url'] = playUrl.strip('$$$')
 
 		result = {
 			'list': [
@@ -133,17 +115,18 @@ class Spider(Spider):
 		}
 		return result
 
-	def verifyCode(self):
+	def verifyCode(self,tag):
 		retry = 10
 		header = {
-			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"}
+			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+		}
 		while retry:
 			try:
 				session = requests.session()
-				img = session.get('https://ikan6.vip/index.php/verify/index.html?', headers=header).content
+				img = session.get('https://ikanys.tv/index.php/verify/index.html?', headers=header).content
 				code = session.post('https://api.nn.ci/ocr/b64/text', data=base64.b64encode(img).decode()).text
-				res = session.post(url=f"https://ikan6.vip/index.php/ajax/verify_check?type=search&verify={code}",
-								   headers=header).json()
+				'https://ikanys.tv/index.php/ajax/verify_check?type=show&verify=0072'
+				res = session.post(url=f"https://ikanys.tv/index.php/ajax/verify_check?type={tag}&verify={code}", headers=header).json()
 				if res["msg"] == "ok":
 					return session
 			except Exception as e:
@@ -152,67 +135,54 @@ class Spider(Spider):
 				retry = retry - 1
 
 	def searchContent(self,key,quick):
+		result = {}
+		header = {
+			"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
+		}
+		url = 'https://ikanys.tv/vodsearch/-------------/?wd={0}'.format(key)
+		session = self.verifyCode('search')
+		rsp = session.get(url, headers=header)
+		root = self.html(rsp.text)
+		vodList = root.xpath("//div[@class='module-items module-card-items']/div")
+		videos = []
+		for vod in vodList:
+			name = vod.xpath("./a/div/div/img/@alt")[0]
+			pic = vod.xpath("./a/div/div/img/@data-original")[0]
+			sid = vod.xpath("./a/@href")[0]
+			sid = self.regStr(sid,"/voddetail/(\\S+)/")
+			videos.append({
+				"vod_id":sid,
+				"vod_name":name,
+				"vod_pic":pic,
+				"vod_remarks":mark
+			})
 		result = {
-				'list': []
+				'list': videos
 			}
 
 		return result
 
 	def playerContent(self,flag,id,vipFlags):
-		result = {}
 		header = {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
-			"Referer": "https://ikan6.vip/"
+			"Referer": "https://ikanys.tv/"
 		}
-		url = 'https://ikan6.vip/{0}'.format(id)
-		rsp = self.fetch(url)
-		cookie = rsp.cookies
-		info = json.loads(self.regStr(reg=r'var player_data=(.*?)</script>', src=rsp.text))
-		string = info['url'][8:len(info['url'])]
-		substr = base64.b64decode(string).decode('UTF-8')
-		str = substr[8:len(substr) - 8]
-		if 'Ali' in info['from']:
-			url = 'https://cms.ikan6.vip/ali/nidasicaibudaowozaina/nicaibudaowozaina.php?url={0}'.format(str)
+		result = {}
+		url = 'https://ikanys.tv/vodplay/{0}/'.format(id)
+		rsp = self.fetch(url, headers=header)
+		info = json.loads(self.regStr(reg=r'var player_aaaa=(.*?)</script>', src=self.cleanText(rsp.text)))
+		parse = 0
+		if info['url'].startswith('http'):
+			purl = info['url']
 		else:
-			url = 'https://cms.ikan6.vip/nidasicaibudaowozaina/nicaibudaowozaina.php?url={0}'.format(str)
-		rsp = self.fetch(url, headers=header, cookies=cookie)
-		randomurl = self.regStr(reg=r"getrandom\(\'(.*?)\'", src=rsp.text)
-		pstring = randomurl[8:len(randomurl)]
-		psubstr = base64.b64decode(pstring).decode('UTF-8')
-		purl = urllib.parse.unquote(psubstr[8:len(psubstr) - 8])
-		result["parse"] = 0
+			parse = 1
+			purl = url
+		result["parse"] = parse
 		result["playUrl"] = ''
 		result["url"] = purl
 		result["header"] = ''
 		return result
-	def get_RegexGetText(self,Text,RegexText,Index):
-		returnTxt="null"
-		Regex=re.search(RegexText, Text, re.M|re.I)
-		if Regex is None:
-			returnTxt="null"
-		else:
-			returnTxt=Regex.group(Index)
-		return returnTxt	
-	def get_RegexGetTextLine(self,Text,RegexText,Index):
-		returnTxt=[]
-		pattern = re.compile(RegexText)
-		ListRe=pattern.findall(Text)
-		if len(ListRe)<1:
-			return returnTxt
-		for value in ListRe:
-			returnTxt.append(value)	
-		return returnTxt
-	def get_playlist(self,Text,headStr,endStr):
-		circuit=""
-		origin=Text.find(headStr)
-		if origin>8:
-			end=Text.find(endStr,origin)
-			circuit=Text[origin:end]
-		return circuit
-	def removeHtml(self,txt):
-		soup = re.compile(r'<[^>]+>',re.S)
-		txt =soup.sub('', txt)
-		return txt.replace("&nbsp;"," ")
+
 	config = {
 		"player": {},
 		"filter": {}
