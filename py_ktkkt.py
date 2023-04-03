c4e9d94934b5a6a3abbd5bb0538224a6d3f9a229
lanhaidixingren <laihaidixingren@163.com>
2023-04-03 07:46:42 +0000

    上传文件至 ''

--- py_ktkkt.py
+++ py_ktkkt.py
@@ -24,7 +24,7 @@ class Spider(Spider):
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"国产动画": "30",
+			"国产动画1": "30",
 			"日韩动画": "3",
 			"国语动画": "1",
 			"粤语动画": "2",
@@ -139,14 +139,14 @@ class Spider(Spider):
 	def playerContent(self,flag,id,vipFlags):
 		result = {}
 		Url='http://ktkkt.top{0}'.format(id)
-		rsp = self.fetch(Url)
+		rsp = self.fetch(Url,headers=self.header)
 		htmlTxt = rsp.text
-		parse=1
+		parse=0
 		UrlStr=self.get_RegexGetText(Text=htmlTxt,RegexText=r'now=base64decode\("(.+?)"\)',Index=1)
 		UrlStr=str(base64.b64decode(UrlStr),'utf-8')
 		if UrlStr.find('.m3u8')<2:
 			Url='http://ktkkt.top{0}'.format(id)
-			parse=0
+			parse=1
 		else:
 			Url=UrlStr
 		result["parse"] = parse
@@ -216,7 +216,8 @@ class Spider(Spider):
 	header = {
 		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
 		'Host': 'ktkkt.top',
-		"Referer": "http://ktkkt.top/"}
+		"Referer": "http://ktkkt.top/"
+	}
 
 	def localProxy(self,param):
 		return [200, "video/MP2T", action, ""]
