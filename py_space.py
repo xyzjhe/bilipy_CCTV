1d242d1b57d29b40786f92044b7b6e4eda615a75
lanhaidixingren <laihaidixingren@163.com>
2023-04-24 08:45:36 +0000

    上传文件至 ''

--- py_space.py
+++ py_space.py
@@ -77,6 +77,9 @@ class Spider(Spider):  # 元类 默认的元类 type
 					reTxt=t
 					break
 			if reTxt!='':
+				rsp = self.fetch(url)
+				htmlTxt=rsp.text
+				line=self.get_RegexGetTextLine(Text=htmlTxt,RegexText=reTxt[1],Index=1)
 				vodItems = [title+"$"+url]
 				#logo=self.get_RegexGetText(Text=htmlTxt,RegexText=reTxt[5],Index=1)
 				
