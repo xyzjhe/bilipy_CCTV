7533f30096a67a713fb344266d4ece9401f5e902
lanhaidixingren <laihaidixingren@163.com>
2023-04-26 07:19:24 +0000

    上传文件至 ''

--- py_space.py
+++ py_space.py
@@ -25,7 +25,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"个人收藏7": "Collection"
+			"个人收藏6": "Collection"
 		}
 		classes = []
 		for k in cateManual:
@@ -71,17 +71,19 @@ class Spider(Spider):  # 元类 默认的元类 type
 		else:
 			id=self.get_RegexGetText(Text=url,RegexText=r'www\.(.+?)\.',Index=1)
 			reTxt=''
-			
+			for t in self.ReStr:
+				if t['name']==id:
+					reTxt=t
+					break
 			if reTxt!='':
-				rsp = self.fetch('http://www.ktkkt2.com/movie/index86485.html')
+				rsp =  self.fetch(url)
 				htmlTxt=rsp.text
-				#line=self.get_RegexGetTextLine(Text=htmlTxt,RegexText=reTxt[1],Index=1)
-				#vod_play_from=['线路','测 试']#[t for t in line]
-				#circuit=self.get_lineList(Txt=htmlTxt,mark=reTxt[2],after=reTxt[3])
+				line=self.get_RegexGetTextLine(Text=htmlTxt,RegexText=reTxt['line'],Index=1)
+				vod_play_from=[t for t in line]
+				circuit=self.get_lineList(Txt=htmlTxt,mark=reTxt['circuit'],after=reTxt['after'])
 				#测试到此
-				vodItems = [str(len(htmlTxt))+"$"+url]
-				#logo=self.get_RegexGetText(Text=htmlTxt,RegexText=reTxt[5],Index=1)
-				
+				for t in circuit:
+					vodItems.append(self.get_EpisodesList(html=t,patternTxt=reTxt['pattern']))
 				#array[0]="{0}###{1}###{2}###{3}".format(tid,title,url,logo)
 		vod = {
 			"vod_id":array[0],
@@ -96,7 +98,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 			"vod_content":""
 		}
 		vod['vod_play_from'] =  "$$$".join(vod_play_from)
-		vod['vod_play_url'] = "#".join(vodItems)
+		vod['vod_play_url'] = "$$$".join(vodItems)
 		result = {
 			'list':[
 				vod
@@ -277,5 +279,15 @@ class Spider(Spider):  # 元类 默认的元类 type
 		'User-Agent':'my.ie.2345.com',
 		'Cookie':'uUiD=35752164629621148571735; name_ie=%2534013%2528023%2522320%2526143%2520154; I=i%3D90475631%26u%3D88890231%26n%3D%25C0%25B6%25BA%25A3%25B5%25D8%25D0%25C7%25C8%25CB%26m%3D0%26t%3D1675499574.1711300%26s%3D6a22a64feb086a03715e47d4cc3e8a29%26v%3D1.1; sData=6392F231FBE75023D053CEFE20A81E6EE43333BF6FF9CD4610BA32AB109E43FD1742EBAAA408F5EB45E7E1A10A40174BC8EE73651C7AD84AC5840AEA48B014F46FE421C992A7799DBF763B0E743AC8716814F0237BC8F8CC62FCF9F8A283040ADD791FBDD3470D699AA43B70F9886350F57021D6DB5E7B8E001ABEFE70C38424; site_str_flag=2; need_modify_name=0; skin=0; theme=0; ggbd=0'
 	}
+	ReStr=[]
+	ReStr.append(vod)
+	vod={
+		'name':'ktkkt2',
+		'line':'<h3 class="title"><strong>(.+?)</strong><span class="text-muted pull-mid">',
+		'circuit':'<div id="video_list_',
+		'after':'</div>',
+		'pattern':r"<li><a title=\'.+?\'\shref=\'(?P<url>.+?)\'"+'\starget="_self">(?P<title>.+?)</a></li>'
+	}
+	ReStr.append(vod)
 	def localProxy(self,param):
 		return [200, "video/MP2T", action, ""]
