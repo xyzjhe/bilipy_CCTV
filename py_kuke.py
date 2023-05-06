d8617067850f250ee1fdc0f9d54e9492b1db0328
lanhaidixingren <laihaidixingren@163.com>
2023-05-06 09:00:53 +0000

    上传文件至 ''

--- py_kuke.py
+++ py_kuke.py
@@ -222,7 +222,15 @@ class Spider(Spider):
 		return circuit
 	config = {
 		"player": {},
-		"filter": {}
+		"dianying":[
+		{"key":"classification","name":"分类","value":[{"n":"全部","v":"dianying"},{"n":"动作片","v":"dongzuopian"}]}
+
+		#分隔
+		]
+
+
+		#分隔
+		}
 	}
 	header = {
 		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
