144668918f59a5a13e94b22e990a0ec7fd212880
lanhaidixingren <laihaidixingren@163.com>
2023-05-20 02:52:43 +0000

    上传文件至 ''

--- py_306ys.py
+++ py_306ys.py
@@ -51,8 +51,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 		videos=[]
 		Url='https://api.web.360kan.com/v1/filter/list?catid={0}&rank=rankhot&cat=&year=&area=&act=&size=35&pageno={1}'.format(tid,pg)
 		self.header['referer']='https://www.360kan.com/dianying/list?rank=rankhot&cat=&year=&area=&act=&pageno=2'#+'2' if pg=='1' else pg
-		rsp = self.fetch('https://agit.ai/lanhaidixingren/Tvbox/raw/branch/master/1.txt')
-		htmlTxt=rsp.text
+		#rsp = self.fetch(Url,cookies=self.header)
+		htmlTxt=webReadFile(urlStr=Url,header=self.header)
 		videos=self.get_list(html=htmlTxt,types=tid)
 		listCount=len(videos)
 		result['list'] = videos
