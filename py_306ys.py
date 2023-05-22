06f432880b82af14cc8725f22e25f43a6654d24b
lanhaidixingren <laihaidixingren@163.com>
2023-05-22 07:32:55 +0000

    上传文件至 ''

--- py_306ys.py
+++ py_306ys.py
@@ -25,7 +25,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			"电视剧1": "2",
+			"电视剧2": "2",
 			"电影": "1",
 			"动漫": "4",
 			"综艺":"3"
@@ -49,7 +49,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 		result = {}
 		videos=[]
 		Url='https://api.web.360kan.com/v1/filter/list?catid={0}&rank=rankhot&cat=&year=&area=&act=&size=35&pageno={1}'.format(tid,pg)
-		self.header['referer']='https://www.360kan.com/dianying/list?rank=rankhot&cat=&year=&area=&act=&pageno={0}'.format('2' if pg=='1' else 9999)
+		self.header['referer']='https://www.360kan.com/dianying/list?rank=rankhot&cat=&year=&area=&act=&pageno={0}'.format('2' if pg=='1' else int(pg)-1)
 		htmlTxt=self.webReadFile(urlStr=Url,header=self.header)
 		videos=self.get_list(html=htmlTxt,types=tid)
 		listCount=len(videos)
@@ -148,7 +148,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 					#print(str(len(vodItems))+x)
 			elif 'playlinksdetail' in data:
 				playlinksdetail=data['playlinksdetail']
-				keyName=[]#list(playlinksdetail.keys())
+				keyName=list(playlinksdetail.keys())
 				for l in keyName:
 					temporary=playlinksdetail[l]
 					url=temporary['default_url']
@@ -191,31 +191,99 @@ class Spider(Spider):  # 元类 默认的元类 type
 				videos.append(title+"$"+url)
 		return videos	
 	def searchContent(self,key,quick):
+		key=urllib.parse.quote(key)
+		url='https://api.so.360kan.com/index?force_v=1&kw={0}&from=&pageno=1&v_ap=1&tab=all'.format(key)
+		self.header['referer']='https://so.360kan.com/?kw={0}&pageNum=1'.format(key)
+		html=self.webReadFile(urlStr=url,header=self.header)
+		#print(url)
+		videos=self.get_list_search(html=html)
+		#print(len(videos))
 		result = {
-			'list':[]
+			'list':videos
 		}
 		return result
+	def get_list_search(self,html):
+		jRoot = json.loads(html)
+		if jRoot['msg']!='ok':
+			return []
+		videos = []
+		data=jRoot['data']
+		if data is None:
+			return []
+		longData=data['longData']
+		if longData is None:
+			return []
+		jsonList=longData['rows']
+		for vod in jsonList:
+			url = vod['en_id']
+			title =vod['titleTxt']
+			img=vod['cover']
+			cat_id=vod['cat_id']
+			cat_name=vod['cat_name']
+			if len(url) == 0:
+				continue
+			guid="{0}###{1}###{2}###{3}".format(cat_id,title,url,img)
+			videos.append({
+				"vod_id":guid,
+				"vod_name":title,
+				"vod_pic":img,
+				"vod_remarks":cat_name
+			})
+		return videos
 	def playerContent(self,flag,id,vipFlags):
 		result = {}
 		headers = {
 			'User-Agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
 		}
-		jx=self.ifJx(urlTxt=id)
-		parse=1
-		if self.get_RegexGetText(Text=id,RegexText=r'(\.mp4)',Index=1)!='':
+		jx=self.ifJx(url=id)
+		parse=jx
+		if self.get_RegexGetText(Text=id,RegexText=r'(cntv|cctv)',Index=1)!='':
+			id=self.get_cctv(id=id)
 			parse=0
+			jx=0
 		result["parse"] = parse
 		result["playUrl"] = ''
 		result["url"] = id
 		result['jx'] = jx#VIP解析
 		result["header"] = headers	
 		return result
-	def ifJx(self,urlTxt):
-		Isjiexi=0
-		RegexTxt=r'(youku.com|v.qq|bilibili|iqiyi.com)'
-		if self.get_RegexGetText(Text=urlTxt,RegexText=RegexTxt,Index=1)!='':
-			Isjiexi=1
+	def ifJx(self,url):
+		Isjiexi=1
+		RegexTxt=r'(cntv|cctv)'
+		if self.get_RegexGetText(Text=url,RegexText=RegexTxt,Index=1)=='':
+			Isjiexi=0
 		return Isjiexi
+	def get_cctv(self,id):
+		result = {}
+		headers={
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
+		"Host": "tv.cctv.com",
+		"Referer": "https://tv.cctv.com/"
+		}
+		htmlTxt=self.webReadFile(urlStr=id,header=headers)
+		pattern = re.compile(r'var\sguid\s*=\s*"(.+?)";')
+		ListRe=pattern.findall(htmlTxt)
+		if ListRe==[]:
+			return result
+		url = "https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={0}".format(ListRe[0])
+		jo = self.fetch(url,headers=self.header).json()
+		link = jo['hls_url'].strip()
+		#rsp = self.fetch(link,headers=self.header)
+		content = self.webReadFile(urlStr=link,header=headers).strip()
+		arr = content.split('\n')
+		urlPrefix = self.regStr(link,'(http[s]?://[a-zA-z0-9.]+)/')
+
+		subUrl = arr[-1].split('/')
+		subUrl[3] = '1200'
+		subUrl[-1] = '1200.m3u8'
+		hdUrl = urlPrefix + '/'.join(subUrl)
+
+		url = urlPrefix + arr[-1]
+
+		hdRsp = self.webReadFile(urlStr=hdUrl,header=headers) 
+		if hdRsp.status_code == 200:
+			url = hdUrl
+		return url
 	def get_RegexGetText(self,Text,RegexText,Index):
 		returnTxt=""
 		Regex=re.search(RegexText, Text, re.M|re.S)
